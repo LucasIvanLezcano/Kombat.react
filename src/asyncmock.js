c923638454d28c277c7cea3b9db5e396f06eb4d8
LucasIvanLezcano <lordvaldomero47@gmail.com>
2023-10-26 15:07:20 -0300

    Second-commit

---
+++ src/asyncmock.js
@@ -0,0 +1,36 @@
+const misProductos = [
+    { id: 1, nombre: "KOMBAT IA-63 PAMPA", precio: 139.999, img: "https://kombatpadel.com.ar/379-thickbox_default/kombat-ia-63-pampa.jpg", idCat: "1" },
+    { id: 2, nombre: "TROYA ULISES", precio: 139.999, img: "https://kombatpadel.com.ar/347-thickbox_default/troya-ulises.jpg", idCat: "1" },
+    { id: 3, nombre: "KOMBAT DELTA FORCE 18K", precio: 139.999, img: "https://kombatpadel.com.ar/335-thickbox_default/kombat-delta-force-18k.jpg", idCat: "1" },
+    { id: 4, nombre: "KOMBAT x GEO", precio: 139.999, img: "https://kombatpadel.com.ar/510-thickbox_default/kombat-x-geo.jpg", idCat: "1" },
+    { id: 5, nombre: "Tubo de bolas Kombat Pádel", precio: 6.99, img: "https://kombatpadel.com.ar/553-thickbox_default/bote-de-bolas-kombat-padel.jpg", idCat: "2" },
+    { id: 6, nombre: "Overgrip individual blanco", precio: 3.000, img: "https://kombatpadel.com.ar/111-thickbox_default/overgrip-individual-blanco-kombat.jpg", idCat: "2" }
+]
+
+
+export const getProductos = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(misProductos);
+        }, 500)
+    })
+}
+
+export const getUnProducto = (id) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const producto = misProductos.find(item => item.id === id);
+            resolve(producto);
+        }, 500)
+    })
+}
+
+
+export const getProductosPorCategoria = (idCategoria) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const productoCategoria = misProductos.filter(item => item.idCat === idCategoria);
+            resolve(productoCategoria);
+        }, 500)
+    })
+}
---
+++ src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import CartWidget from '../CartWidget/CartWidget';
+import './NavBar.css';
+import { Link, NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+
+    <header>
+      <Link to="/">
+        <img className='logo' src={"./imagenes/logo.1.png"} alt="Logo Kombat" />
+      </Link>
+
+      <nav>
+        <ul>
+          <li>
+            <NavLink className='color-nav' to="categoria/1">Palas</NavLink>
+          </li>
+          <li>
+            <NavLink className='color-nav'to="categoria/2">Accesorios</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <CartWidget />
+      
+      
+    </header>
+
+  )
+}
+
+export default NavBar
